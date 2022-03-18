3680c31a743b0f1fbb9764747f7c753f41e96387
javier henao gomez <javierhenaogomez@mac.local>
2022-03-18 15:17:09 -0500

    projecto react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GoogleMap from './components/GoogleMap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Integración Google Maps</h1>
+      <GoogleMap />
+    </div>
+  );
+}
+
+export default App;
